cf69f11ec83f2a5125e93826ac09212e530bda07
Oywayten <oywayten@gmail.com>
2023-05-23 22:03:34 +0300

    Spring Data [#296073] add List<Accident> findAll() to AccidentRepository

--- src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
@@ -1,10 +1,17 @@
 package ru.job4j.accidents.repository;
 
+import lombok.NonNull;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accidents.model.Accident;
+
+import java.util.List;
 
 /**
  * Oywayten 20.05.2023.
  */
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
+
+    @Override
+    @NonNull
+    List<Accident> findAll();
 }
