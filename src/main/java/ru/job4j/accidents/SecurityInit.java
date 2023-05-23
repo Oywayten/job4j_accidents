ae155e23452bb439fa82423ce5d4fdd8f30f408b
Oywayten <oywayten@gmail.com>
2023-05-23 16:02:46 +0300

    Spring Security [#6879]

---
+++ src/main/java/ru/job4j/accidents/SecurityInit.java
@@ -0,0 +1,9 @@
+package ru.job4j.accidents;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Oywayten 23.05.2023.
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
