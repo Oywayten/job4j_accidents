bdfaea5f9f2ade280dada61d6e397e48c190bd5a
Oywayten <oywayten@gmail.com>
2023-05-24 22:40:16 +0300

    Spring test [#6881] add liquibase

--- db/scripts/002_dml_populate_tables_accidents_accident_types_rules_accidents_rules.sql
+++ db/scripts/002_dml_populate_tables_accidents_accident_types_rules_accidents_rules.sql
@@ -5,8 +5,10 @@ VALUES ('Two cars'),
 
 INSERT INTO accidents (name, text, address, accident_type_id)
 VALUES ('Crossroads accident', 'Two cars crashed at an intersection', 'Victory street, 2', 1),
-       ('Pedestrian collision', 'The car was driving on the sidewalk and hit a pedestrian', 'Bucharest street, 39', 2),
-       ('Motorcycle in a ditch', 'The motorcycle did not let the car pass and flew to the side of the road',
+       ('Pedestrian collision',
+        'The car was driving on the sidewalk and hit a pedestrian', 'Bucharest street, 39', 2),
+       ('Motorcycle in a ditch',
+        'The motorcycle did not let the car pass and flew to the side of the road',
         'Country highway, 35 km', 3);
 
 INSERT INTO rules (name)
