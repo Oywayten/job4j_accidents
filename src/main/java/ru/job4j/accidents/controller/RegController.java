d1d4d14b57ec081011b5513293b838d96b63591c
Oywayten <oywayten@gmail.com>
2023-05-25 16:47:11 +0300

    User registration [#296069] add error message

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -2,12 +2,13 @@ package ru.job4j.accidents.controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
-import ru.job4j.accidents.repository.UserRepository;
+import ru.job4j.accidents.service.springdata.UserSpringDataService;
 
 /**
  * Oywayten 23.05.2023.
@@ -16,23 +17,27 @@ import ru.job4j.accidents.repository.UserRepository;
 public class RegController {
 
     private final PasswordEncoder encoder;
-    private final UserRepository users;
+    private final UserSpringDataService userSpringDataService;
     private final AuthorityRepository authorities;
 
     public RegController(
-            PasswordEncoder encoder, UserRepository users, AuthorityRepository authorities) {
+            PasswordEncoder encoder, UserSpringDataService userSpringDataService, AuthorityRepository authorities) {
         this.encoder = encoder;
-        this.users = users;
+        this.userSpringDataService = userSpringDataService;
         this.authorities = authorities;
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, Model model) {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
-        return "redirect:/login";
+        if (!userSpringDataService.save(user)) {
+            String errorMessage = "Username already exist!";
+            model.addAttribute("errorMessage", errorMessage);
+            return "/reg";
+        }
+        return "redirect:/index";
     }
 
     @GetMapping("/reg")
---
+++ src/main/java/ru/job4j/accidents/service/springdata/UserSpringDataService.java
@@ -0,0 +1,30 @@
+package ru.job4j.accidents.service.springdata;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import ru.job4j.accidents.model.User;
+import ru.job4j.accidents.repository.UserRepository;
+
+/**
+ * Oywayten 22.05.2023.
+ */
+@Service
+@Slf4j
+@AllArgsConstructor
+public class UserSpringDataService {
+    private final UserRepository  userRepository;
+
+    public boolean save(User user) {
+        boolean result = false;
+        try {
+            userRepository.save(user);
+            result = true;
+        } catch (DataIntegrityViolationException e) {
+            log.error("Error save user");
+        }
+        return result;
+    }
+}
+
